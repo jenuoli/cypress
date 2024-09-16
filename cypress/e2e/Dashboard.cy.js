ee16276e572d5dd7d95592eebb7d96607912115b
Jenu Oli <jenuoil123@gmail.com>
2024-09-16 22:05:20 +0545

    cypress

--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -1,7 +1,7 @@
 describe('Vendor Registration after otp', function() {
     it('Vendor Registration', function() {
          
-       cy.viewport(1100, 631)
+cy.viewport(1100, 631)
 cy.VendorLogin();
 cy.wait(5000);
 cy.get('.justify-end.gap-2 > :nth-child(2)').click();
